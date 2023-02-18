fd262205446bf03547d8d8f04637e84779470f46
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2023-02-18 01:41:56 -0500

    update

--- pages/mcaic-register.jsx
+++ pages/mcaic-register.jsx
@@ -6,7 +6,7 @@ export default function McaicRegister() {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
                 <title>MCAIC 2023 Registration</title>
                 <script src="https://tally.so/widgets/embed.js" async></script>
-                <style type="text/css">{" html { margin: 0, height: '100%', overflow: 'hidden' } iframe { position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, border: 0 }"}
+                <style type="text/css">{" html { margin: 0, overflow: 'hidden' } iframe { position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, border: 0 }"}
                 </style>
             </Head>
             <body>
